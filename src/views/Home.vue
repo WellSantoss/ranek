18a9e2bd2293c6b1a1cd93334d90ce4d4984ed87
Wellington Santos <wellington1998santoss@gmail.com>
2021-12-24 13:24:56 -0300

    fetch produtos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+
+    <ProdutoBusca />
+    <ProdutoLista />
+  </section>
 </template>
 
 <script>
+import ProdutoBusca from "@/components/ProdutoBusca.vue";
+import ProdutoLista from "@/components/ProdutoLista.vue";
+
 export default {
   name: "Home",
+  components: {
+    ProdutoBusca,
+    ProdutoLista,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
