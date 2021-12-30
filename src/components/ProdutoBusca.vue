f010cb07b6d790beda0babe0e9da69ae6a3d91c2
Wellington Santos <wellington1998santoss@gmail.com>
2021-12-30 12:25:30 -0300

    estilo inputs gerais e de busca

--- src/components/ProdutoBusca.vue
+++ src/components/ProdutoBusca.vue
@@ -1,6 +1,12 @@
 <template>
   <form>
-    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="text"
+      name="busca"
+      id="busca"
+      v-model="busca"
+      placeholder="Buscar..."
+    />
     <button @click.prevent="buscarProdutos" id="lupa">Buscar</button>
   </form>
 </template>
@@ -24,4 +30,33 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -15000px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+}
+</style>
