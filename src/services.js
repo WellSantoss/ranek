c6d94676276a484ba013a218886e0bf23d14f228
Wellington Santos <wellington1998santoss@gmail.com>
2022-10-11 20:31:16 -0300

    deletar produto do usuario

--- src/services.js
+++ src/services.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: 'http://localhost:3000',
 });
 
 export const api = {
@@ -10,6 +10,9 @@ export const api = {
   },
   post(endpoint, body) {
     return axiosInstance.post(endpoint, body);
+  },
+  delete(endpoint) {
+    return axiosInstance.delete(endpoint);
   },
 };
 
